5c09c32b096a154874d9bd84e5bd56e9f1205c6c
Stanislaw Babkin <stalissss@outlook.com>
2022-04-27 22:47:38 +0300

    Create valid in and out from grpc stream

--- pkg/api/mem.go
+++ pkg/api/mem.go
@@ -2,7 +2,6 @@ package api
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	"github.com/Stalis/birdwatch/pkg/api/pb"
@@ -28,22 +27,42 @@ func (m *MemoryServer) GetCurrentMemoryStats(ctx context.Context, req *pb.Curren
 }
 
 func (m *MemoryServer) GetMemoryStats(req *pb.MemoryStatsRequest, srv pb.Memory_GetMemoryStatsServer) error {
-	fmt.Println("Call GetMemoryStats")
+	avgInterval := time.Second * time.Duration(req.Query.AveragingInterval)
+	sendInterval := time.Second * time.Duration(req.Query.SendingInterval)
+
+	watcher := mem.NewWatcher(avgInterval, sendInterval)
+	watcher.Start()
+
+	data := make(chan *mem.MemoryStat)
+	errChan := make(chan error)
+
+	go func(data <-chan *mem.MemoryStat, errChan chan<- error) {
+		for {
+			stat := <-data
+			err := srv.Send(&pb.CurrentMemoryResponse{
+				Total:     int32(stat.Total),
+				Available: int32(stat.Available),
+				Used:      int32(stat.Used),
+			})
+
+			if err != nil {
+				errChan <- err
+				break
+			}
+		}
+	}(data, errChan)
+
 	for {
-		time.Sleep(time.Duration(req.Interval) * time.Millisecond)
+		stat := watcher.Avg()
 
-		stat := mem.GetMemoryStat(srv.Context())
-		err := srv.Send(&pb.CurrentMemoryResponse{
-			Total:     int32(stat.Total),
-			Available: int32(stat.Available),
-			Used:      int32(stat.Used),
-		})
-
-		if err != nil {
+		select {
+		case <-srv.Context().Done():
+			return srv.Context().Err()
+		case err := <-errChan:
 			return err
+		case data <- stat:
 		}
 	}
 
-	fmt.Println("Exit GetMemoryStats")
 	return nil
 }
--- pkg/stat/mem/mem.go
+++ pkg/stat/mem/mem.go
@@ -1,5 +1,12 @@
 package mem
 
+import (
+	"context"
+	"time"
+
+	"github.com/Stalis/birdwatch/pkg/utils"
+)
+
 type MemoryStat struct {
 	Available int
 	Total     int
@@ -21,4 +28,30 @@ type LinuxVMStat struct {
 	MemTotal     int
 	MemFree      int
 	MemAvailable int
+}
+
+type Watcher struct {
+	buffer            *utils.CircleBuffer
+	averagingInterval time.Duration
+	scanInterval      time.Duration
+}
+
+func NewWatcher(averagingInterval time.Duration, scanInterval time.Duration) *Watcher {
+	return &Watcher{
+		buffer:            utils.NewCircleBuffer(int(averagingInterval / scanInterval)),
+		averagingInterval: averagingInterval,
+		scanInterval:      scanInterval,
+	}
+}
+
+func (w *Watcher) Start() {
+
+}
+
+func (w *Watcher) Avg() *MemoryStat {
+	return GetMemoryStat(context.TODO())
+}
+
+func (w *Watcher) Done() {
+
 }
